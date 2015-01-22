042dbac3da5cb4b328fe911ed0d22de847ece4be
ronasta <ronalstal@gmail.com>
2015-01-22 14:48:25 -0300

    first commit

---
+++ population.R
@@ -0,0 +1,10 @@
+# population files downloaded from:
+# - "fedgraph BALT.xls" Baltimore City, MD from http://research.stlouisfed.org/fred2/series/MDBALT5POP#
+#  -"fedgraph LA.xls"   Los Angeles County from http://research.stlouisfed.org/fred2/series/CALOSA7POP#
+
+baFile <- "fedgraph BALT.xls"
+laFile <- "fedgraph LA.xls"
+
+BA <- 
+
+
